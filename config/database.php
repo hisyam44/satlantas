f9f072696e2ce9ca5a808f02cbf24b6c37ab34e0
hisyam44 <mohammadhisyam4@gmail.com>
2016-11-12 21:16:15 -0800

    Change the Database Configuration

--- config/database.php
+++ config/database.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'psql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
