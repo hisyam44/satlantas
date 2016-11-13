034078957584a18662b292754a65e68231d44aec
hisyam44 <mohammadhisyam4@gmail.com>
2016-11-12 21:04:43 -0800

    Prepare to Upload to Server

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -26,6 +26,7 @@ class UserTableSeeder extends Seeder
             $accident->excerpt = "Donec rutrum congue leo eget malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor accumsan tincidunt.";
             $accident->description = "Donec rutrum congue leo eget malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor accumsan tincidunt. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla porttitor accumsan tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Pellentesque in ipsum id orci porta dapibus. Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.";
             $accident->type = "kecelakaan";
+            $accident->photo = "img/example.jpg";
             $accident->latitude = "-6.9143425";
             $accident->longitude = "109.6670824";
             $accident->save();
