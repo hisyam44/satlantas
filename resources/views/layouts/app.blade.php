c5e89795b9b4e89b09882ffef6688c97f24388cd
hisyam44 <mohammadhisyam4@gmail.com>
2016-12-30 23:32:45 +0700

    change Bootstrap url script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700"> -->
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
@@ -89,8 +89,8 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="{{ url('js/jquery.js') }}" crossorigin="anonymous"></script>
-    <script src="{{ url('js/bootstrap.min.js') }}" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="{{ url('js/jquery.js') }}"></script>
+    <script src="{{ url('js/bootstrap.min.js') }}"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
